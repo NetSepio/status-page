839d472f29566cce5bbc2a409f9cfaace8bf03d2
shubham prajapati <shubhamprajapati032@gmail.com>
2025-11-27 00:01:43 +0530

    `Update next.config.js with GitHub Pages settings`

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const production = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  // Required for GitHub Pages static export
+  output: "export",
+
+  // Disable Next.js image optimization (required for next export)
+  images: {
+    unoptimized: true,
+  },
+
+  // (Optional) Asset prefix for GitHub Pages root domain
+  assetPrefix: isProd ? '/' : '',
+};
+
+module.exports = nextConfig;
