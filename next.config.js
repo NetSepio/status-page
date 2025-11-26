85e411eb79ed6d6295f20c57b75b34d6603aaef7
shubham prajapati <shubhamprajapati032@gmail.com>
2025-11-27 00:10:16 +0530

    `Update GitHub Actions workflows and Next.js configuration to use newer versions`

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  assetPrefix: production ? '/' : '',
   reactStrictMode: true,
   swcMinify: true,
+}
 
-  // Required for GitHub Pages static export
-  output: "export",
-
-  // Disable Next.js image optimization (required for next export)
-  images: {
-    unoptimized: true,
-  },
-
-  // (Optional) Asset prefix for GitHub Pages root domain
-  assetPrefix: isProd ? '/' : '',
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
